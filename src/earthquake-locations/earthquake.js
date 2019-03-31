c03553370626784512200cae9df2d657b2278768
Artemee Senin <artemee.senin@gmail.com>
2019-04-01 01:19:45 +0300

    feat: earthquakes drawing

--- src/earthquake-locations/earthquake.js
+++ src/earthquake-locations/earthquake.js
@@ -1,15 +1,52 @@
-import { Observable, from } from 'rxjs';
-import { pipe, flatMap } from 'rxjs/operators'
+import { Observable, from, interval } from 'rxjs';
+import { pipe, flatMap, distinct, retry } from 'rxjs/operators'
 import L from 'leaflet';
 
 const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
 
-function loadJSONP(url){
-    const script = document.createElement("script")
-    script.src = url
+function loadJSONP(settings) {
 
-    const head = document.getElementsByTagName('head')[0];
-    head.appendChild(script)
+  const url = settings.url;
+  const callbackName = settings.callbackName;
+
+  const script = document.createElement("script")
+  script.src = url
+
+  window[callbackName] = data => {
+    window[callbackName].data = data;
+  }
+
+  return Observable.create(observer => {
+    const handler = e => {
+      const status = e.type === 'error' ? 400 : 200;
+      const response = window[callbackName].data;
+
+      if (status === 200) {
+        observer.next({
+          status,
+          responseType: 'jsonp',
+          response,
+          originalEvent: e
+        })
+        observer.complete()
+      } else {
+        observer.error({
+          type: "error",
+          status,
+          originalEvent: e
+        })
+      }
+    }
+
+  script.onload = script.onreadystatechanged = script.onerror = handler;
+
+  const head = document.getElementsByTagName('head')[0];
+
+  if(head.firstChild.nodeName === 'SCRIPT'){
+    head.removeChild(head.firstChild)
+  }
+  head.insertBefore(script, head.firstChild)
+  })
 }
 
 const mapContainer = document.createElement('div')
@@ -20,15 +57,25 @@ const map = L.map('map').setView([33.858631, -118.279602], 7);
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map)
 
 
-const quakes$ = Observable.create(observer => {
-    window.eqfeed_callback = response => {
-        observer.next(response);
-        observer.complete();
-    }
+const quakes$ = interval(5000)
+  .pipe(
+    flatMap(() => {
+      return loadJSONP({
+        url: QUAKE_URL,
+        callbackName: 'eqfeed_callback'
+      }).pipe(
+        retry(3)
+      )
+    }),
+    flatMap(result => {
+      return from(result.response.features);
+    }),
+    distinct( quake => quake.properties.code )
+  )
 
-    loadJSONP(QUAKE_URL);
-}).pipe(
-    flatMap( dataset => {
-        return from(dataset.features);
-    })
-)
+
+  quakes$.subscribe(quake => {
+    const coords = quake.geometry.coordinates;
+    const size = quake.properties.mag * 10000;
+    L.circle([coords[1], coords[0]], size).addTo(map)
+  })
