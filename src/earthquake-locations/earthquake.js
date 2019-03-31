674c6a467a7836589b82c3c9306ad2dd89c1f091
Artemee Senin <artemee.senin@gmail.com>
2019-04-01 00:34:08 +0300

    feat: basic data fetch

--- src/earthquake-locations/earthquake.js
+++ src/earthquake-locations/earthquake.js
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { pipe, flatMap } from 'rxjs/operators'
 import L from 'leaflet';
 
 const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
@@ -17,3 +18,17 @@ document.body.appendChild(mapContainer)
 
 const map = L.map('map').setView([33.858631, -118.279602], 7);
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map)
+
+
+const quakes$ = Observable.create(observer => {
+    window.eqfeed_callback = response => {
+        observer.next(response);
+        observer.complete();
+    }
+
+    loadJSONP(QUAKE_URL);
+}).pipe(
+    flatMap( dataset => {
+        return from(dataset.features);
+    })
+)
