60285f9191561b18886946100d27fcd7d54caec4
Artemee Senin <artemee.senin@gmail.com>
2019-03-31 22:38:15 +0300

    fix: add stylesheets for map

--- src/earthquake-locations/earthquake.js
+++ src/earthquake-locations/earthquake.js
@@ -1,3 +1,19 @@
 import { Observable } from 'rxjs';
+import L from 'leaflet';
 
-console.log('hello my ass')
+const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
+
+function loadJSONP(url){
+    const script = document.createElement("script")
+    script.src = url
+
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(script)
+}
+
+const mapContainer = document.createElement('div')
+mapContainer.id = 'map';
+document.body.appendChild(mapContainer)
+
+const map = L.map('map').setView([33.858631, -118.279602], 7);
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map)
