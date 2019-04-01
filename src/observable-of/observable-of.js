c378bca6f261c82d31579b76d3e8c0410c84530f
Artemee Senin <artemee.senin@gmail.com>
2019-04-01 17:47:01 +0300

    refactor: update intends

--- src/observable-of/observable-of.js
+++ src/observable-of/observable-of.js
@@ -2,11 +2,11 @@ import { Observable, from, of } from 'rxjs';
 import { mergeMap, flatMap } from 'rxjs/operators'
 
 const values$ = from([
-    of(1, 2, 3),
-    of(4, 5, 6),
-    of(7, 8, 9)
-])
+  of(1, 2, 3),
+  of(4, 5, 6),
+  of(7, 8, 9)
+]);
 
 values$.pipe(
-    flatMap( v => v ))
-    .subscribe(v => console.log(v))
+  flatMap( v => v ))
+  .subscribe(v => console.log(v))
--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,30 @@ const { getEntries, getPages } = require('./automation/processPageList');
 var pageList = ['earthquake', 'observable-of'];
 
 module.exports = {
-    entry: {
-        ...getEntries(pageList)
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist'
-    },
-    devServer: {
-        contentBase: ['./dist']
-    },
-    devtool: "source-map",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    "presets": ["@babel/preset-env"]
-                }
-            }
-        }]
-    },
-    plugins: [
-      ...getPages(pageList)
-    ]
+  entry: {
+    ...getEntries(pageList)
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  devServer: {
+    contentBase: ['./dist']
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          "presets": ["@babel/preset-env"]
+        }
+      }
+    }]
+  },
+  plugins: [
+    ...getPages(pageList)
+  ]
 };
